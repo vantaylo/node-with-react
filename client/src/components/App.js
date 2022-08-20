411d1baa33da213005d2e5b3964ae95a325c2101
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2022-08-20 12:33:18 -0500

    created redux store, wired up to component hierarchy using provider tag

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    console.log("Hi There");
+    
+    return (
+        <div>
+            Hi There!
+        </div>
+    );
+};
+
+export default App;
