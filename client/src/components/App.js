263d7aea8cc637660c94a67acf7a2d46abde9e7d
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2022-08-22 18:27:05 -0500

    react router setup, route configs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+// const Header = () => <h2>Header</h2>
+// const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
 
 const App = () => {
     console.log("Hi There");
     
     return (
         <div>
-            Hi There!
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Landing/>}/>
+                    <Route path="/surveys" element={<SurveyNew/>}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
