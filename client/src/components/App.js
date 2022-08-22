93f97d912ac5a959360d603a42a34b2f7fd87d07
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2022-08-22 18:39:46 -0500

    matched routes with exact, always visible components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
-// const Header = () => <h2>Header</h2>
-// const Dashboard = () => <h2>Dashboard</h2>
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
@@ -13,9 +13,12 @@ const App = () => {
     return (
         <div>
             <BrowserRouter>
+            <Header/>
+
                 <Routes>
-                    <Route path="/" element={<Landing/>}/>
-                    <Route path="/surveys" element={<SurveyNew/>}></Route>
+                    <Route exact path="/" element={<Landing/>}/>
+                    <Route exact path="/surveys" element={<Dashboard/>}></Route>
+                    <Route path="/surveys/new" element={<SurveyNew/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
