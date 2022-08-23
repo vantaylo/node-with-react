38376b9e7dac2f249d9216e5917a0c3518d98259
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2022-08-22 21:32:05 -0500

    authreducer returns values, accessing state in the header, header content

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
         
-    dispatchEvent({type: FETCH_USER, payload: res});
+    dispatchEvent({ type: FETCH_USER, payload: res.data });
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderLoginLogoutButton() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href='/auth/google'>Login with Google</a></li>
+                );
+            default:
+                return (<li><a>Logout</a></li>);
+        }
+    }
+
     render() {
         return (
         <nav>
             <div className="nav-wrapper">
                 <a className="left brand-logo"> Logo </a>
                 <ul className="right">
-                    <li>
-                        <a>Login with Google</a>
-                    </li>
+                    {this.renderLoginLogoutButton()}
                 </ul>
             </div>
         </nav>
@@ -17,4 +29,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+    return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
