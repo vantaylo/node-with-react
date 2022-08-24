8ce791f289b204bf86535765edbf0b9b4511240f
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2022-08-24 13:36:22 -0500

    installed stripe checkout, updated node, fixed typo

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
     app.use(
-        ["/api", '/auth/gogle'],
+        ["/api", '/auth/google'],
         createProxyMiddleware({
             target: "http://localhost:3000",
         })
