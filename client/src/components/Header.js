996e62d4b8d4d867fe18c46bcc78bcda45cd7e8a
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2022-08-24 18:53:28 -0500

    created payment component and setup stripe checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payment from './Payment';
 
 class Header extends Component {
     renderLoginLogoutButton() {
@@ -13,7 +14,10 @@ class Header extends Component {
                 );
             default:
                 return (
-                <li><a href='/api/logout'>Logout</a></li>
+                    [
+                    <li id="paymentContainer" ><Payment/></li>,
+                    <li id="logoutBtnContainer"><a href='/api/logout'>Logout</a></li>
+                ]
                 );
         }
     }
