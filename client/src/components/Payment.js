db449f5830d78df16b499925a38808ae85a52c9b
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2022-08-24 19:32:53 -0500

    setup action creator for posting stripe token

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payment extends Component {
     render() {
@@ -8,7 +10,7 @@ class Payment extends Component {
               name="Brand Name"
               description='Purchase Item' 
               amount={500}
-              token={token => console.log(token)}
+              token={token => this.props.handleCheckoutToken(token)}
               stripeKey={process.env.REACT_APP_PUB_STRIPE_KEY}
             >
                 <button className='btn black'>Add Credits</button>
@@ -17,4 +19,4 @@ class Payment extends Component {
     }
 }
 
-export default Payment;
+export default connect(null, actions)(Payment);
