273921a5b975796aaa021db6010e743c6c0a9abf
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2022-08-24 19:13:55 -0500

    action creator to post token to BE

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,3 +6,9 @@ export const fetchUser = () => async dispatch => {
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const handleCheckoutToken = (token) => dispatch => {
+  const res = await axios.post('/api/stripe', token)
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+}
